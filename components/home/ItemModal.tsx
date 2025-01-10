797c12088cf77ba4d1939c3f813634e1035f37d6
jmd348-cavink <cavin.k@jmangroup.com>
2025-01-10 12:06:00 +0530

    feat: added updatestock implementation

--- components/home/ItemModal.tsx
+++ components/home/ItemModal.tsx
@@ -1,6 +1,9 @@
+import { updateStocks } from "@/actions/stockItems";
 import { ItemListUI } from "@/app/(tabs)";
+import { FontAwesome } from "@expo/vector-icons";
 import { useState } from "react";
-import { Modal, View, Text, TextInput, Button, Pressable } from "react-native";
+import { Modal, Pressable, Text, TextInput, View } from "react-native";
+import { ToastAndroid } from "react-native";
 
 export const ItemModal = ({
   isOpen,
@@ -11,6 +14,27 @@ export const ItemModal = ({
   stock: ItemListUI;
   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  const [text, setText] = useState("");
+
+  const updateStockHandler = (type: string) => {
+    if (text.length === 0) {
+      ToastAndroid.show("Enter any number", ToastAndroid.SHORT);
+      return;
+    }
+    const updateStock = type === "inc" ? parseInt(text) : parseInt(text) * -1;
+    const res = updateStocks(stock.id, updateStock);
+    if (res.status === 200) {
+      ToastAndroid.show("Stock Updated Successfully", ToastAndroid.SHORT);
+      setText("");
+      setIsModalOpen(false);
+    } else {
+      ToastAndroid.show(
+        "Stock Update Failed: " + res.error,
+        ToastAndroid.SHORT
+      );
+    }
+  };
+
   return (
     <Modal
       animationType="fade"
@@ -30,33 +54,63 @@ export const ItemModal = ({
               <Text className="text-white text-xl">{stock.count}</Text>
             </View>
           </View>
-          <View className="d-flex flex-row mt-2">
-            <View className="w-[60%]">
-              <TextInput className="bg-white rounded-sm"></TextInput>
+          <View className="d-flex flex-column mt-2">
+            <View className="">
+              <TextInput
+                autoFocus={true}
+                className={`bg-white rounded-sm h-12 text-center ${
+                  text.length > 0 ? "text-lg font-semibold" : "text-slate-400"
+                }`}
+                keyboardType="numeric"
+                value={text}
+                onChangeText={(ch) => {
+                  setText(ch.replace(/[^0-9]+/g, ""));
+                }}
+                placeholder="STOCK SOLD/BOUGHT"
+              ></TextInput>
             </View>
-            <View className="w-[20%]">
-              <Pressable>
+            <View className="d-flex flex-row justify-stretch">
+              <Pressable
+                className="w-[50%]"
+                onPress={() => {
+                  updateStockHandler("inc");
+                }}
+              >
                 <View className="bg-slate-400 p-3">
-                  <Text className="text-center">Inc</Text>
+                  <FontAwesome
+                    size={30}
+                    name="plus-circle"
+                    color="bg-slate-600"
+                    className="text-center"
+                  />
                 </View>
               </Pressable>
-            </View>
-            <View className="w-[20%]">
-              <Pressable>
-                <View className="bg-red-400 p-3">
-                  <Text className="text-center">Dec</Text>
+              <Pressable
+                className="w-[50%]"
+                onPress={() => {
+                  updateStockHandler("dec");
+                }}
+              >
+                <View className="bg-red-500 p-3">
+                  <FontAwesome
+                    size={30}
+                    name="minus-circle"
+                    color="bg-slate-600"
+                    className="text-center"
+                  />
                 </View>
               </Pressable>
             </View>
           </View>
           <Pressable
-            className="mt-2"
+            className="mt-4"
             onPress={() => {
+              setText("");
               setIsModalOpen(!isOpen);
             }}
           >
             <View className="bg-red-500 rounded-md">
-              <Text className="text-center text-white my-2">Close</Text>
+              <Text className="text-center text-white my-2">CLOSE</Text>
             </View>
           </Pressable>
         </View>
